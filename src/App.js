0e7f06c6af80047ad62c7a7e62ae5e1beace52d9
Ermiyas H <Ermi@Ermiyass-MacBook-Pro-2.local>
2022-12-13 21:33:12 -0800

    stocks data display

--- src/App.js
+++ src/App.js
@@ -2,13 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Stocks from './pages/Stocks';
+import Nav from './components/Nav';
 
 function App() {
   return (
     <div className="App">
-      
+      <Nav />
+      <Routes>
+        <Route path='/' element={< Home />} />
+        <Route path='/about' element={< About />} />
+        <Route path='/stocks' element={<Stocks />} />
+      </Routes>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/pages/Price.js
@@ -0,0 +1,14 @@
+import {useEffect, useState} from 'react'
+import {useParams} from 'react-router-dom'
+
+
+function Price () {
+    
+    return (
+      <div className="price">
+        Price list ...
+      </div>
+    );
+  }
+  
+  export default Price;
